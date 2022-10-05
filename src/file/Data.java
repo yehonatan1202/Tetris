ea2805b0b5ab4a23634f4a81942b54dbb8b57600
yehonatan1202 <y12022003@gmail.com>
2022-10-05 22:56:56 +0300

    server&client

---
+++ src/file/Data.java
@@ -0,0 +1,41 @@
+package file;
+
+import java.io.Serializable;
+
+import main.GamePanel;
+import main.Piece;
+
+public class Data implements Serializable {
+	transient GamePanel gamePanel;
+	public int playerTilesMap[][];
+
+	public Data(GamePanel gamePanel) {
+		this.gamePanel = gamePanel;
+		playerTilesMap = new int[gamePanel.playerTileManager.playerTilesMap.length][gamePanel.playerTileManager.playerTilesMap[0].length];
+	}
+
+	public void upload() {s
+		for (int i = 0; i < gamePanel.playerPanel.ScreenRow; i++) {
+			for (int j = 0; j < gamePanel.playerPanel.ScreenCol; j++) {
+				playerTilesMap[i][j] = gamePanel.playerTileManager.playerTilesMap[i][j];
+			}
+		}
+		Piece piece = gamePanel.nextPiecePanel.currentPiece;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (piece.shape[i][j] != 0) {
+					playerTilesMap[i + piece.posY][j + piece.posX] = piece.tile;
+				}
+			}
+		}
+	}
+
+	public void download(GamePanel gamePanel) {
+		for (int i = 0; i < gamePanel.playerPanel.ScreenRow; i++) {
+			for (int j = 0; j < gamePanel.playerPanel.ScreenCol; j++) {
+				gamePanel.opponentTileManager.opponentTilesMap[i][j] = playerTilesMap[i][j];
+			}
+		}
+		// gamePanel.opponenetPanel.currentPiece = playerCurrentPiece;
+	}
+}
